e7cf168af74b2999aa2dded2fb970e1087066403
yluwaca <yluwaca@yahoo.com>
2022-12-29 12:03:02 +0200

    first commit

---
+++ links.py
@@ -0,0 +1,8 @@
+# Creating a List with
+# the use of Numbers
+# code to test that tuples are mutable
+List = [1, 2, 4, 4, 3, 3, 3, 6, 5]
+print("Original list ", List)
+
+List[3] = 77
+print("Example to show mutability ", List)
---
+++ print.py
@@ -0,0 +1,9 @@
+def multipliers(): 
+    return [lambda x : i * x for i in range(4)] 
+    print [m(2) for m in multipliers()]
+
+
+    def multipliers():
+return [lambda x, i=i : i * x for i in range(4)]
+
+print([m(2) for m in multipliers()])
