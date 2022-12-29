5ee914698401b4405a30c880cb85207d694e4155
yluwaca <yluwaca@yahoo.com>
2022-12-29 12:08:08 +0200

    Update print.py

--- print.py
+++ print.py
@@ -1,9 +1,4 @@
-def multipliers(): 
-    return [lambda x : i * x for i in range(4)] 
-    print [m(2) for m in multipliers()]
-
-
-    def multipliers():
-return [lambda x, i=i : i * x for i in range(4)]
+def multipliers():
+    return [lambda x, i=i : i * x for i in range(4)]
 
 print([m(2) for m in multipliers()])
